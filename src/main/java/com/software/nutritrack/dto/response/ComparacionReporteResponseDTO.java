d4115a8a8afaf8541e2f8af7d236b56d6327135c
Maserattew <148399866+Maserattew@users.noreply.github.com>
2025-10-04 21:26:13 -0500

    Merge remote-tracking branch 'origin/feature/report-management' into feature/report-management

--- src/main/java/com/software/nutritrack/dto/response/ComparacionReporteResponseDTO.java
+++ src/main/java/com/software/nutritrack/dto/response/ComparacionReporteResponseDTO.java
@@ -1,10 +1,8 @@
 package com.software.nutritrack.dto.response;
 
-import lombok.Builder;
-
-@Builder
 public record ComparacionReporteResponseDTO(
         Double metaCalorias,
         Double consumido,
-        Integer cumplimiento
+        Long cumplimiento,
+        String mensaje
 ) {}
