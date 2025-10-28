efc9ff93e7285a972f557c30e4ff47ddef92f9c3
Maserattew <148399866+Maserattew@users.noreply.github.com>
2025-10-28 16:11:17 -0500

    Merge remote-tracking branch 'origin/feature/report-management' into feature/report-management

--- src/main/java/com/software/nutritrack/controller/ReporteController.java
+++ src/main/java/com/software/nutritrack/controller/ReporteController.java
@@ -8,7 +8,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import java.time.LocalDate;
-
+import java.util.List;
 @RestController
 @RequestMapping("/api/v1/reports")
 @RequiredArgsConstructor
